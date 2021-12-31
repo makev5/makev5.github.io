2aa59e74b72b2160b7931884fba1214e1a807b2a
MaKe <702164551@qq.com>
2021-12-31 11:05:30 +0800

    Site updated: 2021-12-31 11:05:30

--- js/src/post-details.js
+++ js/src/post-details.js
@@ -101,4 +101,10 @@ $(document).ready(function () {
           NexT.utils.displaySidebar();
       }) : NexT.utils.displaySidebar();
   }
+  $(document).on('click', '.fold_hider', function(){
+    $('>.fold', this.parentNode).slideToggle();
+    $('>:first', this).toggleClass('open');
+  });
+  //默认情况下折叠
+  $("div.fold").css("display","none");
 });
